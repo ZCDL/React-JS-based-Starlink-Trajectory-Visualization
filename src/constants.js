74b87bd704043395d7e1daa6147e0ae08aa4c7fb
ZCDL <67558107+ZCDL@users.noreply.github.com>
2021-08-31 23:50:19 -0700

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const SAT_API_KEY = "48d8fd457ef54145b017107c8bc9b9a8";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
